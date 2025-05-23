0e96d64fb4c611dbab092400d8e20486d4e4144a
dongchunxu <dongchunxu@dongchunxudeMacBook-Pro.local>
2025-05-23 16:27:49 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,10 @@ using namespace std;
 
 int main() {
     int a, b, c;
+    int cc, dd;
     cin >> a >> b >> c;
     int temp = a + b + c;
     cout << temp << endl;
+    int tt, bb;
     return 0;
 }
